bbb34f4eed03cf55d8a7eb2dcaef51215aa27dc2
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-03-16 08:17:35 -0300

    kxxkx

--- exercícios/ex011.py
+++ exercícios/ex011.py
@@ -1,2 +1,5 @@
 n1=int(input('Digite um número para ver o valor da sua tabuada'))
-print('{}x{:2}={}')
+print('{}x{:2}={}'.format(n1,1,n1*1))
+print('{}x{:2}={}'.format(n1,2,n1*2))
+print('{}x{:2}={}'.format(n1,2,n1*3))
+print
