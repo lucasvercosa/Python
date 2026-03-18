ecc39ebdf60f13409659c2c1dcd2709434f8a808
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-03-18 09:02:59 -0300

    jffjjfjc

--- exercícios/ex015.py
+++ exercícios/ex015.py
@@ -1,3 +1,3 @@
-salario=float(input('Qual é o valo do salario?'))
+salario=float(input('Qual é o salário do funcionário?R$'))
 aumento=salario+(salario*15/100)
-print('um funcionario que ganhava {:.2f}')
+print('um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(salario,aumento))
---
+++ exercícios/ex017.py
@@ -0,0 +1,6 @@
+dias=int(input('Quantos dias alugados?'))
+km=float(input('Quantos km rodados?'))
+diaria= dias*60
+percuso= km*0.15
+custo= diaria+percuso
+print()
