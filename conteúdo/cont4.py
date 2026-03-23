7ca17d43b815337244cd84d26aa42e1a456f1d79
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-03-23 12:22:24 -0300

    jcdvmcgmgf

--- conteúdo/cont4.py
+++ conteúdo/cont4.py
@@ -1,3 +1,11 @@
-import math = (input("Digite um número"))
+import math
+num=float(input("Digite um número"))
 raiz=math.sqrt()
-print("A raíz de{} é igual a{}".format(num,raiz))
+print("A raíz de{} é igual a{}".format(num,raiz,math.ceil(raiz)))
+print("A raíz de{} é igual a{}".format(num,raiz,math.floor(raiz)))
+
+#Importando funcionalidades específicas
+#From math import sqrt, ceil, floor
+
+num=int(input)
+raiz=sqrt(num)
