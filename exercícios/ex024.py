4434c7d573426a7cabc4c7d75ff0f202446d4782
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-04-06 09:01:09 -0300

    hgjt

---
+++ exercícios/ex024.py
@@ -0,0 +1,12 @@
+frase = ('curso de análise e desenvolvimento de sistemas')
+print(frase[3]) #isso mostra o espaço da memória
+print(frase[3:46]) #intervalo de caracteres menos o último
+print(frase[3:45:2]) #pula de dois em dois
+print(frase[3:45:2])
+print(frase[7:])
+print(frase[6::9]) #Caractere pulou de três em três
+print(frase.count("m"))
+print(frase.count("o"))
+print(frase.find("sistemas")) #O find apartir de qual indice aparece a pesquisa
+print(frase.find("android"))
+print(frase.in("fala"))
