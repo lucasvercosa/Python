2d526f6e19d418b11afb81c23255545fc1259338
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-05-12 09:01:03 -0300

    Update ex042.py
    dofol

--- exercícios/ex042.py
+++ exercícios/ex042.py
@@ -3,4 +3,7 @@ n2 = float(input("Digite a segunda nota "))
 media = n1+n2/2
 if media < 5:
     print("Reprovado")
-elif media >= 5 and media 
+elif media >= 5 and media <=6.9:
+    print("Recuperação")
+else:
+    print("Aprovado")
