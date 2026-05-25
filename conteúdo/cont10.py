ccfe0eb0dba4c8f544462826d130eb2d1108ceb8
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-05-25 11:00:51 -0300

    ruh

--- conteúdo/cont10.py
+++ conteúdo/cont10.py
@@ -1,5 +1,12 @@
-for se in range (1,6)
-print()
-for se in range (1,6)
-for se in range (7,1)
-print("1")
+for c in range (0,6): #Desconsidera o último
+    print("oi")
+    print("FIM")
+for c in range (0,6):
+    print(c)
+    print("FIM")
+for c in range (6,0,-1): #O -1 faz ele contar em forma decrescente
+    print(c)
+    print("FIM")
+for c in range (0,6,2): #Ele pula de 2 em 2
+    print(c)
+    print("FIM")
---
+++ conteúdo/cont11.py
@@ -0,0 +1,10 @@
+n = int(input("Digite um número: "))
+for c in range (0,n+1): #+1 para chegar o número que eu quero
+    print(c)
+    print("FIM")
+i = int(input("Inicio: "))
+f = int(input("Fim: "))
+p = int(input("Passo: "))
+for c in range (i,f+1,p): #+1 para chegar o número que eu quero
+    print(c)
+    print("FIM")
