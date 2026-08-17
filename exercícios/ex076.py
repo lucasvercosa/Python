f1693a9161b7950d5d52a6f191e6837cc2ef389e
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-08-17 12:02:20 -0300

    kljicsd

---
+++ exercícios/ex076.py
@@ -0,0 +1,5 @@
+from random import randint
+n = (randint(1,10), randint(1,10), randint(1,10), randint(1,10)) #variavel composta
+print(f'Eu sortiei os valores {n}')
+print(f'O maior valor foi {max(n)}') #metodo de tuplas
+print(f'O menor valor foi {min(n)}') #metodo de tuplas
