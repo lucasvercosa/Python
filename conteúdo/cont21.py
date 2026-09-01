a341592609ac6a06be060eeecdfcae2adaf791f1
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2026-09-01 09:02:27 -0300

    gfhdf

---
+++ conteúdo/cont21.py
@@ -0,0 +1,8 @@
+materias=[]
+quimica=[]
+back=[]
+materias.append(quimica[:])
+materias.append(back)
+[[quimica,back]]
+pessoas=[['pedro,75'],['maria,19']]
+print(pessoas)
---
+++ exercícios/ex085.py
@@ -0,0 +1,11 @@
+expr = str(input('Digite uma expressão '))
+esquerda = direita = 0
+for simb in expr: #string é uma lista de caracteres
+    if simb == '(':
+        esquerda +=1
+    elif simb == ')':
+        direita += 1
+if esquerda == direita:
+    print('A expressão esta correta')
+else:
+    print('A expressão não está incorreta')
